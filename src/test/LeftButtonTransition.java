4f8ed902b3ec6b0577338bc7b1bb9b64363e5610
Miharu Tachibana <khem_inw@yahoo.co.jp>
2016-12-10 00:16:26 +0700

    Added first part of long note animation

---
+++ src/test/LeftButtonTransition.java
@@ -0,0 +1,66 @@
+package test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import javafx.animation.Transition;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Transform;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
+
+public class LeftButtonTransition extends Transition {
+	private Polygon polygon;
+	private double[] points;
+	double movablePoint1XOffset;
+	double movablePoint1YOffset;
+	double movablePoint2XOffset;
+	double movablePoint2YOffset;
+	public LeftButtonTransition(Polygon shape) {
+		// TODO Auto-generated constructor stub
+		this.setCycleDuration(Duration.millis(2000));
+		this.polygon = shape;
+		this.points = polygon.getPoints().stream().mapToDouble(d -> d).toArray();
+		movablePoint1XOffset = points[0];
+		movablePoint1YOffset = points[1];
+		movablePoint2XOffset = points[6];
+		movablePoint2YOffset = points[7];
+	}
+
+	public LeftButtonTransition(double arg0, Polygon shape) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+		this.setCycleDuration(Duration.millis(2000));
+		this.polygon = shape;
+	}
+	
+	@Override
+	protected void interpolate(double percentage) {
+		// TODO Auto-generated method stub
+		double[] pointInterpolate = points.clone();
+		double[] currentPoints = points.clone();
+		pointInterpolate[2] = movablePoint1XOffset - pointInterpolate[2];
+		pointInterpolate[3] -= movablePoint1YOffset;
+		pointInterpolate[4] -= movablePoint2XOffset;
+		pointInterpolate[5] -= movablePoint2YOffset;
+		Translate trapezoidTrans = new Translate(30*(float)percentage, 280*(float)percentage);
+		Translate trapezoidTrans2 = new Translate(-130*(float)percentage, 280*(float)percentage);
+		//Scale trapezoidScale = new Scale(6*(float)percentage, 14*(float)percentage);
+		trapezoidTrans.transform2DPoints(pointInterpolate, 2, currentPoints, 2, 1);
+		trapezoidTrans2.transform2DPoints(pointInterpolate, 4, currentPoints, 4, 1);
+		//trapezoidScale.transform2DPoints(pointInterpolate, 2, currentPoints, 2, 2);
+		currentPoints[2] = movablePoint1XOffset - currentPoints[2];
+		currentPoints[3] += movablePoint1YOffset;
+		currentPoints[4] += movablePoint2XOffset;
+		currentPoints[5] += movablePoint2YOffset;
+		
+		for(double point : currentPoints){
+			System.out.println(point);
+		}
+		polygon.getPoints().clear();
+		polygon.getPoints().addAll(Arrays.stream(currentPoints).boxed().collect(Collectors.toList()));
+//		System.out.println(polygon.getPoints());
+	}
+
+}
--- src/test/testTransition.java
+++ src/test/testTransition.java
@@ -17,6 +17,7 @@ import javafx.scene.shape.CubicCurveTo;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.transform.Shear;
@@ -26,8 +27,9 @@ public class testTransition extends Pane {
 
 	public testTransition() {
 		// TODO Auto-generated method stub
-
-		
+		Polygon trapezoid = new Polygon();
+		trapezoid.getPoints().addAll(new Double[] {310.0,70.0,315.0,91.0,295.0,91.0,300.0,70.0});
+		trapezoid.setFill(Color.BLUE);
 		Rectangle rect = new Rectangle();
 		rect.setWidth(10);
 		rect.setHeight(20);
@@ -46,73 +48,65 @@ public class testTransition extends Pane {
 
 		note.setStrokeWidth(10);
 		note.setStrokeLineCap(StrokeLineCap.ROUND);
-		note.setFill(Color.DARKBLUE);
+		note.setFill(Color.BLUE);
+		note.setStroke(Color.BLUE);
 		FadeTransition fadeTransition = new FadeTransition(Duration.millis(3000), note);
 		fadeTransition.setFromValue(0.5f);
 		fadeTransition.setToValue(1f);
 
-		FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(3000), rect);
-		fadeTransition2.setFromValue(0.3f);
+		FadeTransition fadeTransition2 = new FadeTransition(Duration.millis(3000), trapezoid);
+		fadeTransition2.setFromValue(0.5f);
 		fadeTransition2.setToValue(1f);
 
-		TranslateTransition translateTransition = new TranslateTransition(Duration.millis(1900), note);
-		// if(s == 1){
+		TranslateTransition translateTransition = new TranslateTransition(Duration.millis(2000), note);
 		translateTransition.setFromX(0);
 		translateTransition.setToX(-80);
 		translateTransition.setFromY(0);
 		translateTransition.setToY(280);
-		// }
-		TranslateTransition rectr = new TranslateTransition(Duration.millis(2000), rect);
-		rectr.setFromX(0);
-		rectr.setFromY(0);
-		rectr.setToX(-40);
-		rectr.setToY(140);
-		// if(s == 2){
-		// translateTransition.setFromX(0);
-		// translateTransition.setToX(0);
-		// translateTransition.setFromY(0);
-		// translateTransition.setToY(280);
-		// }
-		// translateTransition.setFromX(0);
-		// translateTransition.setToX(80);
-		// translateTransition.setFromY(0);
-		// translateTransition.setToY(280);
+		
+//		TranslateTransition rectr = new TranslateTransition(Duration.millis(2000), trapezoid);
+//		rectr.setFromX(0);
+//		rectr.setFromY(0);
+//		rectr.setToX(-80);
+//		rectr.setToY(140);
 
 		ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(2000), note);
 		scaleTransition.setToX(6);
 		scaleTransition.setToY(2);
 
-		ScaleTransition scaleTransition2 = new ScaleTransition(Duration.millis(2000), rect);
-//		scaleTransition2.setFromX(1);
+//		ScaleTransition scaleTransition2 = new ScaleTransition(Duration.millis(2000), trapezoid);
 //		scaleTransition2.setToX(6);
-//		scaleTransition2.setFromY(1);
 //		scaleTransition2.setToY(14);
-		scaleTransition2.setToX(6);
-		scaleTransition2.setToY(14);
-		TranslateTransition rectr2 = new TranslateTransition(Duration.millis(2000), rect);
-		rectr2.setFromX(-40);
+		
+		LeftButtonTransition left = new LeftButtonTransition(trapezoid);
+		
+		TranslateTransition rectr2 = new TranslateTransition(Duration.millis(2000), trapezoid);
+		rectr2.setFromX(-80);
 		rectr2.setFromY(140);
 		rectr2.setToX(-80);
 		rectr2.setToY(280);
-		ScaleTransition scaleTransition3 = new ScaleTransition(Duration.millis(2000), rect);
+		
+		ScaleTransition scaleTransition3 = new ScaleTransition(Duration.millis(2000), trapezoid);
 		scaleTransition3.setToX(0);
 		scaleTransition3.setToY(0);
-		FadeTransition fadeTransition3 = new FadeTransition(Duration.millis(3000), rect);
+		
+		FadeTransition fadeTransition3 = new FadeTransition(Duration.millis(3000), trapezoid);
 		fadeTransition3.setFromValue(1f);
 		fadeTransition3.setToValue(0f);
 		
 		ParallelTransition parallelTransition = new ParallelTransition();
-		parallelTransition.getChildren().addAll(fadeTransition, translateTransition, scaleTransition, scaleTransition2,
-				rectr, fadeTransition2
-
-		);
+//		parallelTransition.getChildren().addAll(fadeTransition, translateTransition, scaleTransition, scaleTransition2,
+//				rectr, fadeTransition2);
+		parallelTransition.getChildren().addAll(fadeTransition, translateTransition, scaleTransition, left, fadeTransition2);
 		
 		ParallelTransition close = new ParallelTransition();
 		close.getChildren().addAll(rectr2,scaleTransition3,fadeTransition3);
 		SequentialTransition seqT = new SequentialTransition (parallelTransition,close);
-	     seqT.play();
+	    seqT.setCycleCount(Timeline.INDEFINITE); 
+		seqT.play();
 		
-		this.getChildren().addAll(note, rect);
+	     
+		this.getChildren().addAll(note, rect,trapezoid);
 	}
 
 }
